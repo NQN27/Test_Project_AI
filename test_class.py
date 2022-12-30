d05b722828d87a4fddc9e2c203426e7bbf834c23
Luong <116445034+ntluongg@users.noreply.github.com>
2022-12-30 22:40:55 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -42,7 +42,7 @@ class city(object):
         # using a*
         return self.a_star_cost >= other.a_star_cost
 
-    def adjacent_city(self):
+    def adjacent_city(self, matrix):
         adjacent = []
         for i in range(len(matrix[self.id])):
                 if matrix[self.id][i] != 0:
@@ -50,7 +50,6 @@ class city(object):
         return adjacent
 
 def uniform_cost_search(goal, start):
-    global matrix
     q = PriorityQueue()
     
     p = city(start)
@@ -68,7 +67,7 @@ def uniform_cost_search(goal, start):
             return p.ucs_cost, path
         
         if (p.name not in visited):
-            for c in p.adjacent_city():
+            for c in p.adjacent_city(matrix):
                 p1 = city(c)
                 p1.ucs_cost = p.ucs_cost + matrix[p.id][c]
                 p1.path = p.path + [p.name]
@@ -94,7 +93,7 @@ def A_star_search(goal, start):
             return p.ucs_cost, p.path + [p.name]
                 
         if (p.name not in visited):
-            for c in p.adjacent_city():
+            for c in p.adjacent_city(matrix):
                 p1 = city(c, False)
                 p1.ucs_cost = p.ucs_cost + matrix[p.id][c]
                 p1.a_star_cost = p1.ucs_cost + heuristic[p1.id][goal]
@@ -104,6 +103,32 @@ def A_star_search(goal, start):
         visited.append(p.name)
     return None
 
+def uniform_cost_search_time(goal, start):
+    q = PriorityQueue()
+    
+    p = city(start)
+    p.ucs_cost = 0
+
+    q.put(p)
+    visited = [] #mark by list
+
+    while not q.empty():
+        #p is the best node found
+        p = q.get()
+
+        if (p.id == goal):
+            path = p.path + [p.name]
+            return p.ucs_cost, path
+        
+        if (p.name not in visited):
+            for c in p.adjacent_city(time_matrix):
+                p1 = city(c)
+                p1.ucs_cost = p.ucs_cost + time_matrix[p.id][c]
+                p1.path = p.path + [p.name]
+                q.put(p1)
+        
+        visited.append(p.name)
+    return None
 
 def decode(n): 
     global name
@@ -116,11 +141,15 @@ def encode(str):
     return None
 
 
+
 df = pd.read_excel(r"C:\Users\FPTSHOP\Desktop\AI_project\Test_Project_AI\Car_Driving.xlsx")
 df2 = pd.read_excel(r"C:\Users\FPTSHOP\Desktop\AI_project\Test_Project_AI\Air_Distance1.xlsx")
+df3 = pd.read_excel(r"C:\Users\FPTSHOP\Desktop\AI_project\Test_Project_AI\TimeTravel.xlsx")
+
 heuristic = np.array(df2)[::,1:]/1000
 name = np.array(df)[::,0] 
 matrix = np.array(df)[::,1:] /1000
+time_matrix = np.array(df3)[::,1:]
 
 if __name__ == "__main__":
     while True:
@@ -133,6 +162,8 @@ if __name__ == "__main__":
     t2 = time.time()
     answer_a = A_star_search(encode(goal), encode(start))
     t3 = time.time()
+    answer_time = uniform_cost_search_time(encode(goal), encode(start))
+    t4 = time.time()
     if answer == None: #if dont have any solutions
         print('no solution')
 
@@ -159,4 +190,17 @@ if __name__ == "__main__":
             print(i, end='->')        
         print('RUNNING TIME A* SEARCH')
         print("--- %s seconds ---" % (t3 - t2))
+       
+    if answer_time == None: #if dont have any solutions
+        print('no solution')
+
+    else:	
+        solution = answer_time[0]
+        path = answer_time[1]
+        print('ucs minimum time taken from {} to {} is {}'.format(start,goal,solution))
+        print('path :')
+        for i in path:
+            print(i, end='->')        
+        print('RUNNING TIME UCS SEARCH (on time matrix)')
+        print("--- %s seconds ---" % (t4 - t3))
 
