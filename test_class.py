53f3c2705b2441f71a0b82dbc2442c7cc605b469
Luong <116445034+ntluongg@users.noreply.github.com>
2022-12-30 14:02:05 +0700

    cho em update a
    dm thg lz code cho gui mess xong deo copy vao cc fuck u <3

--- test_class.py
+++ test_class.py
@@ -75,7 +75,7 @@ class city(object):
         return adjacent
 
 def uniform_cost_search(goal, start):
-    global graph,cost,path,matrix
+    global matrix
     q = PriorityQueue()
     
     p = city(start)
@@ -90,7 +90,7 @@ def uniform_cost_search(goal, start):
 
         if (p.id == goal):
             path = p.path + [p.name]
-            return p.ucs_cost
+            return p.ucs_cost, path
         
         if (p.name not in visited):
             for c in p.adjacent_city():
@@ -101,6 +101,34 @@ def uniform_cost_search(goal, start):
         
         visited.append(p.name)
     return None
+
+def A_star_search(goal, start):
+    q = PriorityQueue()
+    p = city(start, False, False)
+    p.ucs_cost = 0
+    p.a_star_cost = 0
+
+    q.put(p)
+    visited = [] 
+
+    while not q.empty():
+        #p is the best node found
+        p = q.get()
+
+        if (p.id == goal):
+            return p.ucs_cost, p.path + [p.name]
+                
+        if (p.name not in visited):
+            for c in p.adjacent_city():
+                p1 = city(c, False, False)
+                p1.ucs_cost = p.ucs_cost + matrix[p.id][c]
+                p1.a_star_cost = p1.ucs_cost + heuristic[p1.id][goal]
+                p1.path = p.path + [p.name]
+                q.put(p1)
+
+        visited.append(p.name)
+    return None
+
 
 def decode(n): 
     global name
@@ -113,8 +141,10 @@ def encode(str):
 
 
 if __name__ == '__main__':
-    df = pd.read_excel('/Users/luong/OneDrive - Hanoi University of Science and Technology/project/test_data.xlsx')
-    name = matrix = np.array(df)[::,0] 
+    df = pd.read_excel('/Users/luong/OneDrive - Hanoi University of Science and Technology/project/DRIVE.xlsx')
+    df2 = pd.read_excel('/Users/luong/OneDrive - Hanoi University of Science and Technology/project/AIR.xlsx')
+    heuristic = np.array(df2)[::,1:]
+    name = np.array(df)[::,0] 
     matrix = np.array(df)[::,1:] 
 
     while True:
@@ -122,17 +152,35 @@ if __name__ == '__main__':
         goal = input('GOAL?')
         if (start in name) and (goal in name):
             break
-    start_time = time.time()
-    
+    t1 = time.time()
     answer = uniform_cost_search(encode(goal),encode(start))
+    t2 = time.time()
+    answer_a = A_star_search(encode(goal), encode(start))
+    t3 = time.time()
     if answer == None: #if dont have any solutions
         print('no solution')
 
     else:	
-        print('minimum cost from {} to {} is {}'.format(start,goal,answer))
+        solution = answer[0]
+        path = answer[1]
+        print('ucs minimum cost from {} to {} is {}'.format(start,goal,solution))
         print('path :')
         for i in path:
             print(i, end='->')
+        print('RUNNING TIME UCS SEARCH')
+        print("--- %s seconds ---" % (t2 - t1))
+    print()
+    
+    if answer_a == None: #if dont have any solutions
+        print('no solution')
+
+    else:	
+        solution = answer_a[0]
+        path = answer_a[1]
+        print('a* minimum cost from {} to {} is {}'.format(start,goal,solution))
+        print('path :')
+        for i in path:
+            print(i, end='->')        
+        print('RUNNING TIME A* SEARCH')
+        print("--- %s seconds ---" % (t3 - t2))
 print()
-print('RUNNING TIME UCS SEARCH')
-print("--- %s seconds ---" % (time.time() - start_time))
