9f3023be4b72acb5316984f8f71a24937bdec0b2
NQN27 <104126045+NQN27@users.noreply.github.com>
2023-01-02 23:59:21 +0700

    Update Code_interface.py

--- Code_interface.py
+++ Code_interface.py
@@ -1,6 +1,12 @@
 #Thêm thư viện tkinter
 from tkinter import *
 from tkinter import messagebox
+from test_class import name
+import folium
+from tkinter import Tk, Label, Button
+import webbrowser
+
+
 def Call_interface():
 
     top = Tk()
@@ -33,6 +39,43 @@ def Call_interface():
     #Vị trí xuất hiện của Textbox
     top.mainloop()
     return a,b,c
+def Call_interface2():
+
+    top = Tk()
+    top.title('DIRECTOR')
+    top.geometry("400x250")
+    
+    Start = Label(top, text = "Start").place(x = 30, y = 50)
+    End = Label(top, text = "End").place(x = 30, y = 90)
+    start_point=StringVar()
+    end_point=StringVar()
+    e1 = Entry(top,textvariable=start_point).place(x = 80, y = 50)
+    e2 = Entry(top,textvariable=end_point).place(x = 80, y = 90)
+    def clicked():
+        global a,b
+        a,b=start_point.get(),end_point.get()
+        if a not in name or b not in name:
+            messagebox.showwarning('WRONG NAME', 'Please write corect name of the city like HaNoi,HoChiMinhCity')
+        else:
+            top.destroy()
+    btn = Button(top, text="Submit", bg="white", fg="red",command=clicked)
+
+    #Thiết lập vị trí của nút nhấn có màu nền và màu chữ
+    btn.place(x=250, y=65)
+    #Vị trí xuất hiện của Textbox
+    top.mainloop()
+    return a,b
+def show(m):
+    root = Tk()
+    root.geometry('800x600')
+    def show_map():
+        m.save('map.html')
+        webbrowser.open_new('map.html')
+    button = Button(root, text="Show Map", command=show_map)
+    button.pack()
+    root.mainloop()
+
+
 
 
 #Lặp vô tận để hiển thị cửa sổ
