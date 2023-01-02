ee2ac3a28145be423d9c8c9c4e9b70ea61c359fd
Luong <116445034+ntluongg@users.noreply.github.com>
2023-01-02 21:01:03 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from queue import PriorityQueue
 import time
+from queue import PriorityQueue
 
 class city(object):
     def __init__(self, id, using_ucs = True):
@@ -41,6 +41,12 @@ class city(object):
             return self.ucs_cost >= other.ucs_cost 
         # using a*
         return self.a_star_cost >= other.a_star_cost
+    
+    def __str__(self):
+        str = ''
+        for i in self.path:
+            str = str + i + '-'
+        return str
 
     def adjacent_city(self, matrix):
         adjacent = []
@@ -50,11 +56,13 @@ class city(object):
         return adjacent
 
 def uniform_cost_search(goal, start):
+    global expanded_routes_U, expanded_nodes_U
+
     q = PriorityQueue()
     
     p = city(start)
     p.ucs_cost = 0
-
+    p.path = [p.name]
     q.put(p)
     visited = [] #mark by list
 
@@ -63,51 +71,95 @@ def uniform_cost_search(goal, start):
         p = q.get()
 
         if (p.id == goal):
-            path = p.path + [p.name]
-            return p.ucs_cost, path
+            return p.ucs_cost, p.path
         
         if (p.name not in visited):
             for c in p.adjacent_city(matrix):
                 p1 = city(c)
                 p1.ucs_cost = p.ucs_cost + matrix[p.id][c]
-                p1.path = p.path + [p.name]
+                p1.path = p.path + [p1.name]
                 q.put(p1)
+                
+                expanded_routes_U.append(p1) #for drawing nodes and routes
+                if p1.name in expanded_nodes_U:
+                    expanded_nodes_U[p1.name] = expanded_nodes_U[p1.name] + 1
+                else:
+                    expanded_nodes_U[p1.name] = 0 
         
         visited.append(p.name)
     return None
 
 def A_star_search(goal, start):
+    global expanded_routes_A1, expanded_nodes_A1
     q = PriorityQueue()
-    p = city(start, False)
+    p = city(start, using_ucs = False)
     p.ucs_cost = 0
     p.a_star_cost = 0
-
+    p.path = [p.name]
     q.put(p)
     visited = [] 
-
     while not q.empty():
         #p is the best node found
         p = q.get()
 
         if (p.id == goal):
-            return p.ucs_cost, p.path + [p.name]
+            return p.ucs_cost, p.path
                 
         if (p.name not in visited):
             for c in p.adjacent_city(matrix):
-                p1 = city(c, False)
+                p1 = city(c, using_ucs = False)
                 p1.ucs_cost = p.ucs_cost + matrix[p.id][c]
-                p1.a_star_cost = p1.ucs_cost + 2*heuristic[p1.id][goal]
-                p1.path = p.path + [p.name]
+                p1.a_star_cost = p1.ucs_cost + heuristic[p1.id][goal]
+                p1.path = p.path + [p1.name]
                 q.put(p1)
+
+                expanded_routes_A1.append(p1) #for drawing nodes and routes
+                if p1.name in expanded_nodes_A1:
+                    expanded_nodes_A1[p1.name] = expanded_nodes_A1[p1.name] + 1
+                else:
+                    expanded_nodes_A1[p1.name] = 0 
 
         visited.append(p.name)
     return None
 
-def uniform_cost_search_time(goal, start,start_time):
+def A_star_search2(goal, start):
+    global expanded_routes_A2, expanded_nodes_A2
+    q = PriorityQueue()
+    p = city(start, using_ucs = False)
+    p.ucs_cost = 0
+    p.a_star_cost = 0
+    p.path = [p.name]
+    q.put(p)
+    visited = [] 
+    while not q.empty():
+        #p is the best node found
+        p = q.get()
+
+        if (p.id == goal):
+            return p.ucs_cost, p.path
+                
+        if (p.name not in visited):
+            for c in p.adjacent_city(matrix):
+                p1 = city(c, using_ucs = False)
+                p1.ucs_cost = p.ucs_cost + matrix[p.id][c]
+                p1.a_star_cost = p1.ucs_cost + 2*heuristic[p1.id][goal]
+                p1.path = p.path + [p1.name]
+                q.put(p1)
+
+                expanded_routes_A2.append(p1) #for drawing nodes and routes
+                if p1.name in expanded_nodes_A2:
+                    expanded_nodes_A2[p1.name] = expanded_nodes_A2[p1.name] + 1
+                else:
+                    expanded_nodes_A2[p1.name] = 0 
+
+        visited.append(p.name)
+    return None
+
+def uniform_cost_search_time(goal, start):
     q = PriorityQueue()
     
     p = city(start)
-    p.ucs_cost = start_time
+    p.ucs_cost = 0
 
     q.put(p)
     visited = [] #mark by list
@@ -142,14 +194,20 @@ def encode(str):
 
 
 
-df = pd.read_excel(r"Car_Driving.xlsx")
-df2 = pd.read_excel(r"Air_Distance1.xlsx")
-df3 = pd.read_excel(r"TimeTravel.xlsx")
-
+df = pd.read_excel(r"C:\Users\FPTSHOP\Desktop\AI_project\Test_Project_AI\Car_Driving.xlsx")
+df2 = pd.read_excel(r"C:\Users\FPTSHOP\Desktop\AI_project\Test_Project_AI\Air_Distance1.xlsx")
+df3 = pd.read_excel(r"C:\Users\FPTSHOP\Desktop\AI_project\Test_Project_AI\TimeTravel.xlsx")
 heuristic = np.array(df2)[::,1:]
-name = np.array(df)[::,0] 
-matrix = np.array(df)[::,1:] /1000
+name = np.array(df)[::,0]
+matrix = np.array(df)[::,1:]/1000
 time_matrix = np.array(df3)[::,1:]
+
+expanded_nodes_U = dict()
+expanded_routes_U = []
+expanded_nodes_A1 = dict()
+expanded_routes_A1 = []
+expanded_nodes_A2 = dict()
+expanded_routes_A2 = []
 
 if __name__ == "__main__":
     while True:
@@ -162,11 +220,11 @@ if __name__ == "__main__":
     t2 = time.time()
     answer_a = A_star_search(encode(goal), encode(start))
     t3 = time.time()
-    answer_time = uniform_cost_search_time(encode(goal), encode(start),0)
+    answer_time = uniform_cost_search_time(encode(goal), encode(start))
     t4 = time.time()
+
     if answer == None: #if dont have any solutions
         print('no solution')
-
     else:	
         solution = answer[0]
         path = answer[1]
@@ -176,8 +234,10 @@ if __name__ == "__main__":
             print(i, end='->')
         print('RUNNING TIME UCS SEARCH')
         print("--- %s seconds ---" % (t2 - t1))
+        print('UCS* nodes opened: ',expanded_nodes_U)
+        print('total = ',sum(expanded_nodes_U.values()),' nodes')
+        print('UCS* number of routes : ',len(expanded_routes_U))
     print()
-    
     if answer_a == None: #if dont have any solutions
         print('no solution')
 
@@ -190,7 +250,11 @@ if __name__ == "__main__":
             print(i, end='->')        
         print('RUNNING TIME A* SEARCH')
         print("--- %s seconds ---" % (t3 - t2))
-       
+        print('A* nodes opened: ',expanded_nodes_A1)
+        print('total = ',sum(expanded_nodes_A1.values()),' nodes')
+        print('A* number of routes : ',len(expanded_routes_A1))
+    print()
+
     if answer_time == None: #if dont have any solutions
         print('no solution')
 
@@ -203,4 +267,3 @@ if __name__ == "__main__":
             print(i, end='->')        
         print('RUNNING TIME UCS SEARCH (on time matrix)')
         print("--- %s seconds ---" % (t4 - t3))
-
